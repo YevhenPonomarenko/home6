9df24776ae84e821632fa43d4690ce00ecdd5a8c
YevhenPonomarenko <yevhenponomarenko99@gmail.com>
2020-06-20 21:09:28 +0300

    home6

---
+++ home/2/Validate.js
@@ -0,0 +1,19 @@
+﻿function validate(elem, pattern) {
+    var res = pattern.test(elem.value);
+    if (res === false) {
+        elem.className = "invalid";
+    }
+    else {
+        elem.className = "valid";
+    }
+}
+
+function sendForm() {
+    for (var i = 0; i < form1.elements.length; ++i) {
+        if (form1.elements[i].className === "invalid") {
+            alert("Допущены ошибки при заполнении формы.");
+            event.preventDefault();
+            return;
+        }
+    }
+}
